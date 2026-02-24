74704d9dfbd17bd12e113941938b7523f0ece324
Nathan Lanza <nathanlanza@gmail.com>
2026-02-23 17:13:21 -0800

    Forward kitty push/pop/set to all clients viewing the window

--- input.c
+++ input.c
@@ -2114,7 +2114,7 @@ input_csi_dispatch_kitk_push(struct input_ctx *ictx)
 		if (w == NULL)
 			return;
 
-		/* Find a client attached to this window */
+		/* Forward kitty flags enable to all clients attached to this window */
 		TAILQ_FOREACH(c, &clients, entry) {
 			if (c->session != NULL &&
 			    c->session->curw != NULL &&
@@ -2124,7 +2124,6 @@ input_csi_dispatch_kitk_push(struct input_ctx *ictx)
 				xsnprintf(seq, sizeof seq, "\033[>%du", flags);
 				tty_puts(&c->tty, seq);
 				tty_puts(&c->tty, "\033[?u");
-				break;
 			}
 		}
 	}
@@ -2172,7 +2171,6 @@ input_csi_dispatch_kitk_pop(struct input_ctx *ictx)
 			    c->session->curw->window == w &&
 			    c->tty.term != NULL) {
 				tty_puts(&c->tty, "\033[<u");
-				break;
 			}
 		}
 	}
@@ -2236,7 +2234,6 @@ input_csi_dispatch_kitk_set(struct input_ctx *ictx)
 				xsnprintf(seq, sizeof seq, "\033[=%d;1u",
 				    new_flags);
 				tty_puts(&c->tty, seq);
-				break;
 			}
 		}
 	}
